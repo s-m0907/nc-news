702c2e1ddc78fd0aa334e0a4c193aaa330ade206
s-m0907 <suzewhaites@gmail.com>
2024-02-20 14:31:37 +0000

    GET /api/articles/:article_id/comments and  /api/articles refactor

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,24 @@
 const db = require("../db/connection.js");
 const app = require("../app.js");
 const seed = require("../db/seeds/seed.js");
+const sorted = require("jest-sorted");
 const { topicData, userData, articleData, commentData } = require("../db/data/test-data/index.js");
 const request = require("supertest");
 
-beforeEach(() => seed({topicData, userData, articleData, commentData }));
+beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 
 afterAll(() => db.end());
+
+describe("404 Invalid Endpoint", () => {
+  test("GET:404 responds with status code and error message when passed an invalid endpoint", () => {
+    return request(app)
+      .get("/api/not-a-route")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Not found");
+      });
+  });
+});
 
 describe("CORE: GET /api/topics", () => {
   test("GET:200 responds with an array of topic objects", () => {
@@ -15,7 +27,7 @@ describe("CORE: GET /api/topics", () => {
       .expect(200)
       .then((response) => {
         const topics = response.body.topics;
-        expect(topics.length).toBe(3)
+        expect(topics.length).toBe(3);
         topics.forEach((topic) => {
           expect(topic).toHaveProperty("slug");
           expect(topic).toHaveProperty("description");
@@ -31,89 +43,118 @@ describe("CORE: GET /api", () => {
       .expect(200)
       .then((response) => {
         expect(typeof response.body).toBe("object");
-        expect(response.body).toHaveProperty("endpoints")
+        expect(response.body).toHaveProperty("endpoints");
       });
   });
 });
 
 describe("CORE: GET /api/articles/:article_id", () => {
-    test("GET:200 responds with the article object for the given id", () => {
-      return request(app)
-        .get("/api/articles/1")
-        .expect(200)
-        .then((response) => {
-          const article = response.body.article;
-          expect(article).toHaveProperty("title");
-          expect(article).toHaveProperty("topic");
-          expect(article).toHaveProperty("author");
-          expect(article).toHaveProperty("body");
-          expect(article).toHaveProperty("created_at");
-          expect(article).toHaveProperty("article_img_url");
-        });
-    });
-    test("GET:404 responds with appropriate status code and message when passed an valid but nonexistant id", () => {
-      return request(app)
-        .get("/api/articles/999")
-        .expect(404)
-        .then((response) => {
-          expect(response.body.msg).toBe('Article does not exist');
-        });
-    });
-    test('GET:400 responds with appropriate status code and message when passed an invalid id', () => {
-      return request(app)
+  test("GET:200 responds with the article object for the given id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        const article = response.body.article;
+        expect(article).toHaveProperty("title");
+        expect(article).toHaveProperty("topic");
+        expect(article).toHaveProperty("author");
+        expect(article).toHaveProperty("body");
+        expect(article).toHaveProperty("created_at");
+        expect(article).toHaveProperty("article_img_url");
+      });
+  });
+  test("GET:404 responds with appropriate status code and message when passed an valid but nonexistant id", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Article does not exist");
+      });
+  });
+  test("GET:400 responds with appropriate status code and message when passed an invalid id", () => {
+    return request(app)
       .get("/api/articles/four")
       .expect(400)
       .then((response) => {
-          expect(response.body.msg).toBe('Bad request')
-      })
-    });
+        expect(response.body.msg).toBe("Bad request");
+      });
   });
+});
 
-  describe('CORE: GET /api/articles', () => {
-    test('GET:200 responds with an array of all article objects with correct properties', () => {
-        return request(app)
-        .get("/api/articles")
-        .expect(200)
-        .then((response) => {
-            const articles = response.body.articles
-            articles.forEach((article) => {
-                expect(article).toHaveProperty('author')
-                expect(article).toHaveProperty('title')
-                expect(article).toHaveProperty('article_id')
-                expect(article).toHaveProperty('topic')
-                expect(article).toHaveProperty('created_at')
-                expect(article).toHaveProperty('votes')
-                expect(article).toHaveProperty('article_img_url')
-                expect(article).not.toHaveProperty('body')
-                expect(article).toHaveProperty('comment_count')
-
-            })
-        })
-    });
-    test('GET:200 article array is sorted by date descending as a default', () => {
-      return request(app)
+describe("CORE: GET /api/articles", () => {
+  test("GET:200 responds with an array of all article objects with correct properties", () => {
+    return request(app)
       .get("/api/articles")
       .expect(200)
       .then((response) => {
-        const articles = response.body.articles
-        const oldestArticle = articles[articles.length -1]
-        expect(oldestArticle.article_id).toBe(7)
-      })
-    });
-    test('GET:400 responds with status code and error message when passed an invalid sort by', () => {
-      return request(app)
-      .get("/api/articles?sort_by=flavour")
-      .expect(400)
-      .then((response) => {
-        expect(response.body.msg).toBe("Invalid sort query")
-      })
-    });
-    test('GET:400 responds with status code and error message when passed invalid order query', () => {
-      return request(app)
-      .get("/api/articles?order=alphabetical")
-      .expect(400)
-      .then((response) => {
-        expect(response.body.msg).toBe("Invalid order query")
-      })
-    });
+        const articles = response.body.articles;
+        articles.forEach((article) => {
+          expect(article).toHaveProperty("author");
+          expect(article).toHaveProperty("title");
+          expect(article).toHaveProperty("article_id");
+          expect(article).toHaveProperty("topic");
+          expect(article).toHaveProperty("created_at");
+          expect(article).toHaveProperty("votes");
+          expect(article).toHaveProperty("article_img_url");
+          expect(article).not.toHaveProperty("body");
+          expect(article).toHaveProperty("comment_count");
+        });
+      });
   });
+  test("GET:200 article array is sorted by date descending as a default", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        const articles = response.body.articles;
+        expect(articles).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+});
+
+describe("CORE:GET /api/articles/:article_id/comments", () => {
+  test("GET:200 responds with an array of comments with correct properties for the given article_id", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then((response) => {
+        const comments = response.body.comments;
+        comments.forEach((comment) => {
+          expect(comment).toHaveProperty("comment_id");
+          expect(comment).toHaveProperty("votes");
+          expect(comment).toHaveProperty("created_at");
+          expect(comment).toHaveProperty("author");
+          expect(comment).toHaveProperty("body");
+          expect(comment).toHaveProperty("article_id");
+        });
+        expect(comments.length).toBe(11);
+      });
+  });
+  test("GET:200 responds an array of comments sorted by most recent comment", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then((response) => {
+        const comments = response.body.comments;
+        expect(comments).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("GET:404 responds with appropriate status code and message when passed an valid but nonexistant id", () => {
+    return request(app)
+      .get("/api/articles/999/comments")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Article does not exist");
+      });
+  });
+  test("GET:400 responds with appropriate status code and message when passed an invalid id", () => {
+    return request(app)
+      .get("/api/articles/twenty/comments")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad request");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const { getTopics, getEndpoints, getArticleById, getArticles } = require("./controller");
-const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require("./errors.controller.js")
+const { getTopics, getEndpoints, getArticleById, getArticles, getCommentsByArticleId } = require("./controller.js");
+const { handleCustomErrors, handlePsqlErrors, handleServerErrors, handleInvalidEndpoint } = require("./errors.controller.js")
 const app = express();
-
-app.use(express.json());
 
 app.get('/api/topics', getTopics)
 
@@ -12,6 +10,10 @@ app.get('/api', getEndpoints)
 app.get('/api/articles/:article_id', getArticleById)
 
 app.get('/api/articles', getArticles)
+
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
+
+app.all("/*", handleInvalidEndpoint)
 
 app.use(handleCustomErrors)
 
