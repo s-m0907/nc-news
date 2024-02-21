430f4246b12ee57d6698527b3bf7c6ce0b66eb36
s-m0907 <suzewhaites@gmail.com>
2024-02-21 15:58:40 +0000

    refactor task 7 - adding more tests for post error handling

--- errors.controller.js
+++ errors.controller.js
@@ -7,7 +7,11 @@ exports.handleCustomErrors = (err, req, res, next) => {
 exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
-  } else next(err);
+  }
+  else if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found" })
+  } 
+  else next(err);
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
