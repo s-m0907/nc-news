7699439dc2b48b6314699227e5077f6195fded2a
s-m0907 <suzewhaites@gmail.com>
2024-02-22 15:49:43 +0000

    refactor removeComment in model

--- models.js
+++ models.js
@@ -14,9 +14,11 @@ exports.readEndpoints = () => {
 };
 
 exports.selectArticle = (article_id) => {
-  
   return db
-    .query("SELECT articles.author, title, articles.body, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id)::INT AS comment_count FROM articles FULL JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url", [article_id])
+    .query(
+      "SELECT articles.author, title, articles.body, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id)::INT AS comment_count FROM articles FULL JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url",
+      [article_id]
+    )
     .then((result) => {
       if (!result.rows[0]) {
         return Promise.reject({
@@ -29,18 +31,18 @@ exports.selectArticle = (article_id) => {
 };
 
 exports.selectArticles = (topic) => {
-  const validTopics = ['mitch', 'cats', 'paper']
-  const queryValues = []
-  
+  const validTopics = ["mitch", "cats", "paper"];
+  const queryValues = [];
+
   let queryString = `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id)::INT AS comment_count
-  FROM articles FULL JOIN comments ON articles.article_id = comments.article_id`
-  
-  if(topic) {
-    if(!validTopics.includes(topic)) {
-      return Promise.reject({status: 404, msg: 'Topic not found'})
+  FROM articles FULL JOIN comments ON articles.article_id = comments.article_id`;
+
+  if (topic) {
+    if (!validTopics.includes(topic)) {
+      return Promise.reject({ status: 404, msg: "Topic not found" });
     }
-    queryString += ` WHERE topic = $1`
-    queryValues.push(topic)
+    queryString += ` WHERE topic = $1`;
+    queryValues.push(topic);
   }
 
   queryString += `
@@ -55,8 +57,8 @@ exports.selectArticles = (topic) => {
 exports.selectCommentsByArticle = (article) => {
   let queryString = `SELECT * FROM comments`;
   const queryValues = [];
-    queryValues.push(article);
-    queryString += ` WHERE article_id = $1 ORDER BY created_at DESC`;
+  queryValues.push(article);
+  queryString += ` WHERE article_id = $1 ORDER BY created_at DESC`;
 
   return db.query(queryString, queryValues).then((result) => {
     if (!result.rows[0]) {
@@ -88,39 +90,43 @@ exports.insertComment = (newComment, article) => {
 };
 
 exports.updateVotes = (votes, article) => {
-  return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article])
-  .then((result) => {
-    if(!result.rows[0]) {
-      return Promise.reject({
-        status: 404,
-        msg: `Article not found`
-      })
-    }
-  }).then(() => {
-  return db.query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 AND votes + $1 >= 0 RETURNING *`, [votes, article])
-  .then((result) => {
-      if(!result.rows[0]) {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article])
+    .then((result) => {
+      if (!result.rows[0]) {
         return Promise.reject({
-          status: 400,
-          msg: `Bad request`,
+          status: 404,
+          msg: `Article not found`,
         });
       }
-    return result.rows[0]
-  })
-})
-}
+    })
+    .then(() => {
+      return db
+        .query(
+          `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 AND votes + $1 >= 0 RETURNING *`,
+          [votes, article]
+        )
+        .then((result) => {
+          if (!result.rows[0]) {
+            return Promise.reject({
+              status: 400,
+              msg: `Bad request`,
+            });
+          }
+          return result.rows[0];
+        });
+    });
+};
 
 exports.removeComment = (comment) => {
   return db
-  .query("SELECT * FROM comments WHERE comment_id = $1;", [comment])
-  .then((result) => {
-    if (!result.rows[0]) {
-      return Promise.reject({
-        status: 404,
-        msg: `Comment not found`,
-      });
-    }
-  return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment])
-})
-}
-
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *`, [comment])
+    .then((result) => {
+      if (!result.rows[0]) {
+        return Promise.reject({
+          status: 404,
+          msg: `Comment not found`,
+        });
+      }
+    });
+};
